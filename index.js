72c6cb0ebe906705d65d5bbaba600e354a580d63
Adam Baldwin <adamnbaldwin@gmail.com>
2023-08-15 11:20:18 -0700

    update win/lose logic and add win tally section

--- index.js
+++ index.js
@@ -1,77 +1,68 @@
-const choices = ["rock", "paper", "scissors"];
-function getComputerChoice(){
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-function getplayerChoice(){
-    let validatedInput = false;
-    while (validatedInput == false){
-    const choice = prompt("Rock, Paper, Scissors?");
-    if (choice == null){
-        continue;
-    }   
-    const choiceInLower = choice.toLowerCase(); 
-    if(choices.includes(choiceInLower)){
-        validatedInput = true;
-        return choiceInLower;
-    }
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const winTally = document.querySelector("#tallyText");
+const winTallyComputer = document.querySelector("#tallyTextComputer");
+const finalResult = document.querySelector("#finalResult");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+let playerTally = 0;
+let computerTally = 0;
 
-    }
-}
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-function checkWinner (playerSelection, computerSelection) {
-    if(playerSelection == computerSelection){
-        return "Tie";
-    } else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper") 
-    ){
-        return "Player";
-        
-    } else if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock") 
-    ){
-        return "Computer";
-    } 
-}
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = `Result: ${checkWinner()}`;
+    tallyWins();
+    winTally.textContent = `Player Wins: ${playerTally}`;
+    winTallyComputer.textContent = `Computer Wins: ${computerTally}`;
+    winnerCheck();
+    finalResult.textContent = `${winnerCheck()}`;
 
-function playRound(playerSelection, computerSelection){
-    const result = checkWinner(playerSelection, computerSelection);
-    if(result == "Tie"){
-        return "It's a tie"
-    } else if (result == "Player"){
-        return `Player wins - ${playerSelection} beats ${computerSelection}`
-    } else if (result == "Computer"){
-        return `Computer wins - ${computerSelection} beats ${playerSelection}`
-    }
-    }
+}));
 
-function game(){
-   let scorePlayer = 0;
-   let scoreComputer = 0;
-    console.log("Welcome friend")
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getplayerChoice();
-        const computerSelection = getComputerChoice();  
-        console.log(playRound(playerSelection, computerSelection));
-        if(checkWinner(playerSelection,computerSelection) == "Player") {
-            scorePlayer++;
-        } else if (checkWinner(playerSelection,computerSelection) == "Computer") {
-            scoreComputer++;
-        } else if (checkWinner(playerSelection, computerSelection) == "Tie" ) {
-            scorePlayer++ && scoreComputer++;
-        }
-    }
-    console.log("Game Over Friend!");
-    if(scorePlayer > scoreComputer) {
-        console.log(`Player wins best of 5!`)
-    } else if(scoreComputer > scorePlayer) {
-        console.log(`Computer wins best of 5!`)
-    } else {
-        console.log("It is a tie, yo")
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computer = "Rock";
+        break;
+      case 2:
+        computer = "Paper";
+        break;
+      case 3:
+        computer = "Scissors";
+        break;
     }
 }
+function checkWinner(){
+    if(player == computer){
+      return "Draw";
+    }
+    else if(computer == "Rock"){
+      return (player == "Paper") ? "Player Wins" : "Computer Wins"
+    }
+    else if(computer == "Paper"){
+      return (player == "Scissors") ? "Player Wins" : "Computer Wins"
+    }
+    else if(computer == "Scissors"){
+      return (player == "Rock") ? "Player Wins" : "Computer Wins"
+    }
+}; 
 
-game()
+function tallyWins(){
+if (checkWinner()=="Player Wins") {playerTally++;}
+else if (checkWinner()=="Computer Wins") {computerTally++};}
+
+function winnerCheck(){
+  if (playerTally > 4) {return "GAME OVER, PLAYER IS CHAMPION";}
+  else if (computerTally > 4) {return "GAME OVER, COMPUTER IS CHAMPION";}
+  else {return "First to 5 wins is champion!"};
+  };
